6b2707b26258a247fca6d96c67a24fe4c1f0e2b8
Roi malka <roimalka10@gmail.com>
2022-09-26 02:07:14 +0300

    added a contact call to action button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./css/main-styles.css";
 import Header from "./components/Header";
+import ContactButton from "./components/ContactButton";
+import HomeGallery from "./components/HomeGallery";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <ContactButton />
+      <HomeGallery />
     </div>
   );
 };
---
+++ src/components/ContactButton.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../css/ContactButton.css";
+import { motion } from "framer-motion";
+
+const ContactButton = () => {
+  return (
+    <>
+      <a href="#" className="contact-link">
+        <svg
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          width="20px"
+          height="20px"
+          viewBox="0 0 20 20"
+          enable-background="new 0 0 20 20"
+          space="preserve"
+        >
+          <path
+            fill="#ffffff"
+            d="M16,17H4a2,2,0,0,1-2-2V5A2,2,0,0,1,4,3H16a2,2,0,0,1,2,2V15A2,2,0,0,1,16,17Zm0-2V7.619l-6,4.242L4,7.619V15H16ZM4,5V5.705l6,3.429,6-3.429V5H4Z"
+          ></path>
+        </svg>
+        <span>Contact me</span>
+      </a>
+    </>
+  );
+};
+
+export default ContactButton;
