7f3fad29ab9ffde52ea07ff321ea2e660cf2fd4e
Roi malka <roimalka10@gmail.com>
2022-09-26 02:06:35 +0300

    header changes and fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,8 +16,8 @@ const Header = () => {
   return (
     <motion.div
       className="header"
-      initial={{ y: "-100vh" }}
-      animate={{ y: 0 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
       transition={{ duration: 0.6, delay: 0.6, type: "tween" }}
     >
       <Link
