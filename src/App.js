42812176ae8d6a582c666d3b06a3d8d4a95a2452
Roi malka <roimalka10@gmail.com>
2022-09-27 03:51:58 +0300

    main css and app file changes + new dependencies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./css/main-styles.css";
+import "./css/UpButton.css";
 import Header from "./components/Header";
 import ContactButton from "./components/ContactButton";
 import HomeGallery from "./components/HomeGallery";
+import Projects from "./components/Projects";
+import NewsList from "./components/NewsList";
+import ScrollToTop from "react-scroll-to-top";
+import { ImArrowUp2 } from "react-icons/im";
 
 const App = () => {
   return (
@@ -10,6 +15,9 @@ const App = () => {
       <Header />
       <ContactButton />
       <HomeGallery />
+      <Projects />
+      <NewsList />
+      <ScrollToTop smooth className="up-button" component={<ImArrowUp2 />} />
     </div>
   );
 };
