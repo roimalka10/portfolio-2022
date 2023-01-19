0910706c7dc0ff5a8c0f624c32dda814ed91fc4d
Roi malka <roimalka10@gmail.com>
2023-01-20 01:38:37 +0200

    removed usescroll from template

--- src/containers/ProjectsTemplate.js
+++ src/containers/ProjectsTemplate.js
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import PageHeight from "../Reusables/PageHeight";
-import { motion, useScroll, useSpring } from "framer-motion";
+import { motion } from "framer-motion";
 
 const animations = {
   exit: {
@@ -13,12 +13,6 @@ const animations = {
 
 const ProjectsTemplate = ({ projectRef }) => {
   const [pageHeight] = PageHeight();
-  const { scrollYProgress } = useScroll();
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 200,
-    damping: 20,
-    restDelta: 0.001,
-  });
 
   return (
     <motion.div
@@ -28,7 +22,6 @@ const ProjectsTemplate = ({ projectRef }) => {
       exit="exit"
       transition={{ duration: 0.5 }}
     >
-      <motion.div className="progress-bar" style={{ scaleX }} />
       <Header projectRef={projectRef} />
       <Outlet />
       <Footer />
